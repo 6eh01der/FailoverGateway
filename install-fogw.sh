764ef8f6b3d91fe88e3c66a30a9ae8fa840e141c
IBeholderI <ras_atari@mail.ru>
2021-01-18 20:07:57 +0300

    Update install-fogw.sh

--- install-fogw.sh
+++ install-fogw.sh
@@ -7,8 +7,8 @@ cp fogw.timer /usr/lib/systemd/system/
 read -p "Set gateways and ping timeout separated by spaces. First - default gateway, second - backup gateway, third - timeout in seconds like 1,(for example: 192.168.1.1 192.168.1.2 1): " ARGUMENTS
 sed -i "6s/$/ $ARGUMENTS/" /etc/systemd/system/fogw.service
 systemctl daemon-reload
-read -p "Enable automatic failback to default gateway when it comes up? (Yes/No) " ANSWER
-if [ $ANSWER == No ]
+read -p "Enable automatic failback to default gateway when it comes up? (yes/no) " ANSWER
+if [ $ANSWER == no ]
 then
 sed -i "41a : '" /opt/fogw/fogw.sh
 sed -i "54a '" /opt/fogw/fogw.sh
