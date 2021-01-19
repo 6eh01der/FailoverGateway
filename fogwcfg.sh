b715fdeab34005ba2d4f96ae2fe838fea5518754
IBeholderI <ras_atari@mail.ru>
2021-01-19 15:02:28 +0300

    Update fogwcfg.sh
    milliseconds instead seconds

--- fogwcfg.sh
+++ fogwcfg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-read -p "Set gateways and ping timeout separated by spaces. First - default gateway, second - backup gateway, third - timeout in seconds like 1,(for example: 192.168.1.1 192.168.1.2 1): " ARGUMENTS
+read -p "Set gateways and ping timeout separated by spaces. First - default gateway, second - backup gateway, third - timeout in milliseconds like 500(default value),(for example: 192.168.1.1 192.168.1.2 700): " ARGUMENTS
 sed -i "s/^ExecStart.*$/ExecStart=\/opt\/fogw\/fogw.sh $ARGUMENTS/" /etc/systemd/system/fogw.service
 systemctl daemon-reload
