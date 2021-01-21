a8bcdab077286a25a0ccf48d51b6a68b85010f63
IBeholderI <ras_atari@mail.ru>
2021-01-21 10:54:32 +0300

    Update fogwcfg.sh
    As auto failback now sets as fourth argument now config script able to change it

--- fogwcfg.sh
+++ fogwcfg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-read -p "Set gateways and ping timeout separated by spaces. First - default gateway, second - backup gateway, third - timeout in milliseconds like 500(default value),(for example: 192.168.1.1 192.168.1.2 700): " ARGUMENTS
+#Version: 1.1.2
+#https://github.com/IBeholderI/FailoverGateway/blob/main/fogwcfg.sh
+
+read -p "Set gateways, ping timeout and auto failback on/off separated by spaces. First - default gateway, second - backup gateway, third - timeout in milliseconds like 700, fourth - auto failback on/off,(for example: 192.168.1.1 192.168.1.2 700 on): " ARGUMENTS
 sed -i "s/^ExecStart.*$/ExecStart=\/opt\/fogw\/fogw.sh $ARGUMENTS/" /etc/systemd/system/fogw.service
 systemctl daemon-reload
