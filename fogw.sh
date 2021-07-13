6aab65ba59f771d80cce6202ad4c740006239cf8
Artur Lebedev <ras_atari@mail.ru>
2021-07-13 12:51:21 +0300

    Simplification of current gateway address check condition

--- fogw.sh
+++ fogw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Version: 1.1.5
+#Version: 1.1.6
 #https://github.com/IBeholderI/FailoverGateway/blob/main/fogw.sh
 #*********************************************************************
 #       Configuration
@@ -74,8 +74,7 @@ off)
                 fi
 ;;
 esac
-elif [ "$CURT_GW" != "$DEF_GW" ] && [ "$CURT_GW" != "$BCK_GW" ]
-then
+else
 echo "fogw configured gateways are different then currently used. Current default gateway must be empty or the same as default or backup gateway placed in fogw config or maybe fogw gateways config is wrong."
 exit 1
 fi
